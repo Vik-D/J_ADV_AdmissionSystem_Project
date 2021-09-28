6a0a281194c23aff62a0fd7f982af498f59aeb42
Vik-D <didykvik@outlook.com>
2021-09-28 20:34:07 +0300

    dao
    update on repositories(manual)

--- J_ADV_AdmissionSystem_Project/src/main/java/ua/lviv/lgs/project/dao/ApplicantProfileRepository.java
+++ J_ADV_AdmissionSystem_Project/src/main/java/ua/lviv/lgs/project/dao/ApplicantProfileRepository.java
@@ -1,10 +1,16 @@
 package ua.lviv.lgs.project.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import ua.lviv.lgs.project.domain.ApplicantProfile;
 
+@Repository
 public interface ApplicantProfileRepository extends JpaRepository<ApplicantProfile, Integer> {
  
+	Optional<ApplicantProfile> findById(Integer id);
+	
 }
  
